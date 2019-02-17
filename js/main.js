4e11f2a41d06323a5a754529b4786fb1e0dfd488
jellyforjully088@yahoo.com <jellyforjully088@yahoo.com>
2019-02-17 15:24:21 +0100

    helo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,67 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const fs = require("fs");
+const _ = require("lodash");
 
-(function () {
-  var compareDate = new Date('Feb 18 2019 00:00:00');
-  var curDate = new Date();
-  if(curDate.getTime() > compareDate.getTime()){
-    window.location.replace('http://www.google.com')
-    console.log(curDate);
-    console.log(compareDate);
-  }else {
-        window.location.href="http://www.yahoo.com";
-  }
-})();
+const { window } = new JSDOM(
+  `
+<body>
+    <script src='./node_modules/ccapture.js/build/CCapture.all.min.js'></script>
+    <canvas id='animation' width='400' height='200'></canvas>
+</body>
+`,
+  // We need these options to allow JSDOM to require CCapture from node_modules
+  { runScripts: "dangerously", resources: "usable" }
+);
+
+const document = window.document;
+
+// Do our stuff after DOM is ready.
+window.onload = () => {
+  const canvas = document.getElementById("animation");
+  const ctx = canvas.getContext("2d");
+
+  // Doing some random animation here
+  const render = () => {
+    ctx.fillStyle = "blue";
+    ctx.font = "30px Impact";
+    ctx.rotate(_.random(0.1, 0.2));
+    ctx.fillText("Awesome!", 50, 100);
+  };
+
+  // Framerate for capturer is 1 per second just for example
+  const capturer = new window.CCapture({
+    format: "png",
+    framerate: 1,
+    verbose: true
+  });
+
+  capturer.start();
+
+  // Doing 3 renders, and capture the canvas
+  const interval = setInterval(() => {
+    render();
+    capturer.capture(canvas);
+  }, 1000);
+
+  // Now clearing the interval, stopping capturer
+  setTimeout(() => {
+    clearInterval(interval);
+
+    capturer.stop();
+
+    // Saving the file using FileReader (from JSDOM) and node.js API
+    capturer.save(blob => {
+      const reader = new window.FileReader();
+      reader.onload = () => {
+        const arrayBuffer = reader.result;
+        const uint8Array = new Uint8Array(arrayBuffer);
+
+        // Sync for simplicity
+        fs.writeFileSync("./images.tar", uint8Array, { encoding: "binary" });
+      };
+
+      reader.readAsArrayBuffer(blob);
+    });
+  }, 4000);
+};
